a7c0acd6a8761e18ef4253dd198e9ab804ac0146
Tarun Bylapudi <bylasoft@gmail.com>
2023-10-18 00:41:30 +0530

    connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+//connect to MongoDb
+connectDB();
 
 const app = express();
 
