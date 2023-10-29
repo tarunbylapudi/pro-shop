dfa0cb5cc3e050764f4b19cb556fb03624143776
Tarun Bylapudi <bylasoft@gmail.com>
2023-10-29 18:13:11 +0530

    alais update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import ProductCard from "../components/ProductCard";
 import { useGetProductsQuery } from "../slices/productSlice";
 
 const HomePage = () => {
-  const { data, isLoading, error } = useGetProductsQuery();
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
@@ -16,7 +16,7 @@ const HomePage = () => {
         <>
           <h1 className="mt-3">Latest Products</h1>
           <Row>
-            {data.map((product) => (
+            {products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <ProductCard product={product} />
               </Col>
